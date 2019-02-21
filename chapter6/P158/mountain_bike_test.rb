f2cbef624c65ec042b769d79f5e0a098780bc548
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-02-21 12:27:03 +0900

    chapter6 super: no superclass method `spares' for #<MountainBike:0x007fd3449b7b10>

--- chapter6/P158/mountain_bike_test.rb
+++ chapter6/P158/mountain_bike_test.rb
@@ -11,4 +11,14 @@ class MountainBikeTest < Minitest::Unit::TestCase
     )
     assert_equal('S', mountain_bike.size)
   end
+
+  # Minitest::UnexpectedError: NoMethodError: super: no superclass method `spares' for #<MountainBike:0x007fd3449b7b10>
+  def test_spares
+    mountain_bike = MountainBike.new(
+        size: 'S',
+        front_shock: 'Manitou',
+        rear_shock: 'Fox'
+    )
+    mountain_bike.spares
+  end
 end
