34daa2492e48a786f68d526d3c68a9cde2cff596
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-02-21 12:31:35 +0900

    RoadBikeからattributeを昇格

--- chapter6/P158/bicycle.rb
+++ chapter6/P158/bicycle.rb
@@ -5,9 +5,11 @@ class Bicycle
   # 継承の難しさは、抽象から具象を厳密に分けることに失敗すること
   # 全てサブクラスに下げないと、具象がスーパークラスにに残ることになってしまう
   # 具象の見落としは致命的だが、抽象の見落としは後で可視化される
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args={})
     @size = args[:size] # RoadBikeから昇格
+    @chain = args[:chain] # sparesを昇格させるため
+    @tire_size = args[:tire_size] # sparesを昇格させるため
   end
 end
