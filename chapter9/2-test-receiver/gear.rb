80616b3aa44a70f50d2ac115ace0e2e7767dd6a6
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-02-24 15:13:13 +0900

    9.2 テスト対象のオブジェクトを隔離する

--- chapter9/2-test-receiver/gear.rb
+++ chapter9/2-test-receiver/gear.rb
@@ -1,20 +1,27 @@
 require_relative './wheel'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
-    @rim = args[:rim]
-    @tire = args[:tire]
+    @wheel = args[:wheel]
   end
 
   # 受信メッセージ gear_inches に応答する
   def gear_inches
-    # Wheel に対する diameter という送信メッセージ
-    # diameter に依存している
-    ratio * Wheel.new(rim, tire).diameter
+    # wheel変数内にあるオブジェクトが diameter に応答する
+    # Diameterizable ロールを担うことになった
+
+    # Gearを作る際には Diameterizable を注入する必要が出てきた
+    # しかし、Wheelは diameter に応答するというこのロールに答える唯一のクラス
+    # アプリケーションとして動かす際には毎回Wheelインスタンスを注入する必要がある
+    #
+    # 更に、Wheelを直接注入するか、Diameterizableを注入することは、プログラマの考えが大きく違う
+    # クラスに縛られない想像力をDiameterizableは可能にしている
+    # 注入するオブジェクトがロールのインスタンスであると考える -> Diameterizable についてより多くの選択肢が考えられる
+    ratio * wheel.diameter
   end
 
   # 受信メッセージ ratio に応答する
--- chapter9/2-test-receiver/gear_test.rb
+++ chapter9/2-test-receiver/gear_test.rb
@@ -1,16 +1,15 @@
 require_relative './gear'
+require_relative './wheel'
 require "minitest/autorun"
 
 class GearTest < Minitest::Unit::TestCase
 
-  # 一見、WheelTestと同様の見た目だが、gear.gear_inchesの先で、
-  # Wheelをオブジェクトを作成し、使用している
+  # Wheel のインスタンスをテスト中に注入している
   def test_calculate_gear_inches
     gear = Gear.new(
                    chainring: 52,
                    cog: 11,
-                   rim: 26,
-                   tire: 1.5
+                   wheel: Wheel.new(26, 1.5)
     )
     assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
