9debfd272378c1d99f9ed3ea3088940fbea628ca
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-03-09 07:16:49 +0900

    スタブを用意するためのサブクラスを作る

--- chapter9/6-test-Inheritance/bicycle_test.rb
+++ chapter9/6-test-Inheritance/bicycle_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require_relative './bicycle'
 require_relative './bicycle_interface_test'
+require_relative './stubbed_bike'
 
 class BicycleTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
@@ -13,9 +14,19 @@ class BicycleTest < MiniTest::Unit::TestCase
     # Bicycleは抽象クラス、newメッセージを受け取ることを想定していない
     # → 綺麗な作成プロトコルはない、アプリケーション上は必要ない
     @bike = @object = Bicycle.new({tire_size: 0})
+    @stubbed_bike = StubbedBike.new
   end
 
   def test_forces_subclasses_to_implement_default_tire_size
     assert_raises(NotImplementedError) {@bike.default_tire_size}
+  end
+
+  def test_include_local_spares_in_spares
+    assert_equal @stubbed_bike.spares,
+                 {
+                     tire_size: 0,
+                     chain: '10-speed',
+                     saddle: 'painful'
+                 }
   end
 end
