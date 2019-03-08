e742b7d6eafe05675e272fd33f36486d4fdf4005
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-03-09 07:10:29 +0900

    具象サブクラスの特化をテストする

--- chapter9/6-test-Inheritance/road_bike_test.rb
+++ chapter9/6-test-Inheritance/road_bike_test.rb
@@ -10,4 +10,11 @@ class RoadBikeTest < MiniTest::Unit::TestCase
   def setup
     @bike = @object = RoadBike.new({size: 'L', tape_color: 'red'})
   end
+
+  # RoadBikeはlocal_sparesを実装しているので、sparesに応答する
+  # しかし、テストではその知識を持ち出すべきではない
+  # あくまで、local_sparesが動作することを保証するべき
+  def test_puts_tape_color_in_local_spares
+    assert_equal 'red', @bike.local_spares[:tape_color]
+  end
 end
