371303273182e7d4b4659b51294b5ad7b6d67e62
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-03-09 06:41:40 +0900

    サブクラスの要件を満たしていることを証明する

--- chapter9/6-test-Inheritance/road_bike_test.rb
+++ chapter9/6-test-Inheritance/road_bike_test.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
 require_relative './road_bike'
 require_relative './bicycle_interface_test'
+require_relative './bicycle_subclass_test'
 
 class RoadBikeTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
+  include BicycleSubclassTest
 
   def setup
     @bike = @object = RoadBike.new({size: 'L', tape_color: 'red'})
